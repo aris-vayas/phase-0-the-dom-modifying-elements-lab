05435d4bd6317e0a4f356c37bed341d5d491cbac
Aris Vayas <aris.vayas@gmail.com>
2022-04-08 10:08:06 -0400

    commit tree ad9e6d73b1416d1b683ed64f6ba4c167bd027c8c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+const element = document.getElementById("main")
+element.remove("main")
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id',"victory")
+newHeader.textContent ="is the champion"
+document.body.append(newHeader)
+
+
+
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul)
